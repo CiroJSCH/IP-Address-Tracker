5a7a18873d9968a254bc40a76ab69d671556d84f
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-27 19:53:25 -0300

    useRef para IP

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,14 +1,25 @@
+import { useContext, useEffect, useRef } from "react";
+import { LocationContext } from "../context/LocationContext";
+
 import "../styles/searcher.css";
 
 const Searcher = () => {
+
+  const { setIp } = useContext(LocationContext);
+  const ipInput = useRef("");
+
+  const ipHandler = () => {
+    setIp(ipInput.current.value);
+  }
+
   return (
     <div className="searcher">
       <h1>IP Address Tracker</h1>
       <div className="search">
         <div className="search__input">
-          <input type="text" name="ip" placeholder="Search for any IP address or domain"/>
+          <input type="text" name="ip" placeholder="Search for any IP address or domain" ref={ipInput}/>
         </div>
-        <div className="search__button">
+        <div className="search__button" onClick={ipHandler}>
           <span></span>
         </div>
       </div>
