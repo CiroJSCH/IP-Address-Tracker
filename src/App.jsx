b4c1421ad631066225938325aff3c661020bb9a9
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-27 19:53:11 -0300

    ContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 // Components
 import Searcher from "./components/Searcher";
 import Result from "./components/Result";
+import Map from "./components/Map";
+
+// Context
+import CartContextProvider from "./context/LocationContext";
 
 // Styles
 import "./styles/reset.css";
@@ -9,10 +13,13 @@ import "./styles/styles.css";
 const App = () => {
   return (
     <div className="App">
-      <Searcher />
-      <Result />
+      <CartContextProvider>
+        <Searcher />
+        <Result />
+        <Map />
+      </CartContextProvider>
     </div>
-  )
-}
+  );
+};
 
 export default App;
