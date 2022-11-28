97f9ed7019f2732245bdf370504bd9fb0bdb7df5
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-28 11:06:44 -0300

    Estados para la informacion y error

--- src/context/LocationContext.jsx
+++ src/context/LocationContext.jsx
@@ -1,3 +1,4 @@
+// Libraries
 import { createContext, useState } from "react";
 
 export const LocationContext = createContext();
@@ -5,12 +6,18 @@ export const LocationContext = createContext();
 const LocationContextProvider = ({ children }) => {
 
   const [ip, setIp] = useState("");
+  const [information, setInformation] = useState({});
+  const [error, setError] = useState(false);
 
   return (
     <LocationContext.Provider
       value={{
         ip,
         setIp,
+        information,
+        setInformation,
+        error,
+        setError,
       }}
     >
       {children}
